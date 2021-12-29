0fd1f6c7cae5f6a1d193bbb8efc58ed6e2b90a62
Francisco Fetapi <franciscofetapi10@gmail.com>
2021-12-29 13:27:45 +0000

    InfoUsuario criado

--- src/components/Perfil/Main.jsx
+++ src/components/Perfil/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Perfil } from "../../styles/pages/Perfil";
 import Banner from "./Banner";
+import InfoUsuario from "./InfoUsuario";
 
 export default function Main() {
   return (
     <Perfil.Container>
       <Banner />
+      <InfoUsuario />
     </Perfil.Container>
   );
 }
--- src/styles/pages/Perfil.js
+++ src/styles/pages/Perfil.js
@@ -7,6 +7,66 @@ export const Perfil = {
     margin: 0 auto;
   `,
   Main: styled.div``,
+  Info: styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 30px;
+    width: 90%;
+    margin: 0 auto;
+    margin-bottom: 30px;
+    .foto-nome-bio {
+      display: grid;
+      grid-template-columns: auto 1fr;
+      column-gap: 20px;
+      figure {
+        position: relative;
+        width: 90px;
+        height: 90px;
+        margin: 5px;
+        top: -25px;
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+        }
+        figcaption {
+          position: absolute;
+          bottom: 0;
+          right: 0;
+          div {
+            padding: 5px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: #716e6e;
+            border-radius: 50%;
+            svg {
+              color: white;
+            }
+          }
+        }
+      }
+    }
+    .mais_info_perfil {
+      .MuiListItemIcon-root {
+        min-width: auto;
+        margin-right: 10px;
+        svg {
+          font-size: 30px;
+        }
+        .home {
+          color: #fa2f3d;
+          font-size: 37px;
+        }
+        .thumbUp {
+          color: #388e3c;
+        }
+        .thumbDown {
+          color: #df8e3c;
+        }
+      }
+    }
+  `,
   Banner: styled.div`
     width: 100%;
     height: 300px;
