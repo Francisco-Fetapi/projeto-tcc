8e8b2418944eb03e4d46fc34bd74debe1f85ba51
Francisco Fetapi <franciscofetapi10@gmail.com>
2021-12-30 14:22:07 +0000

    botão CARREGAR MAIS em FAVORITOS criado

--- src/components/Perfil/Favoritos.jsx
+++ src/components/Perfil/Favoritos.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import List from "@material-ui/core/List";
+import Button from "@material-ui/core/Button";
 import { Text } from "../../styles";
 import Favorito from "./Favorito";
 
@@ -14,6 +15,9 @@ export default function Favoritos() {
         {["matrix.jpg", "TWD.jpg", "lucifer.jpg"].map((item) => (
           <Favorito key={item} imgMovie={item} />
         ))}
+      </Box>
+      <Box mt={1} display="flex" justifyContent="center">
+        <Button color="primary">Carregar mais</Button>
       </Box>
     </List>
   );
