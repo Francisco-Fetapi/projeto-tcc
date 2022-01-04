402707d8d19d9380e4b15f001ae822db804de635
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-01-04 16:22:11 +0000

    form Editar perfil criado

--- src/components/Forms/FormEditarPerfil.jsx
+++ src/components/Forms/FormEditarPerfil.jsx
@@ -2,38 +2,203 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import TextField1 from "../TextField1";
 import Box from "@material-ui/core/Box";
+import MenuItem from "@material-ui/core/MenuItem";
 
-import { Formik, Form } from "formik";
+import Radio from "@material-ui/core/Radio";
+import RadioGroupMui from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+
+import AlternateEmail from "@material-ui/icons/AlternateEmail";
+
+import { Formik, Form, useField } from "formik";
 import useUsuario from "../../hooks/useUsuario";
+import paises from "../../mock/paises.json";
+import generos from "../../mock/generos.json";
+import motivos from "../../mock/motivosAssistir.json";
+import { Text } from "../../styles";
+import { FaUser } from "react-icons/fa";
+
+function RadioGroup({ children, id }) {
+  const [field] = useField({ name: id });
+  return <RadioGroupMui {...field}>{children}</RadioGroupMui>;
+}
 
 export default function FormAddPost() {
   const { usuario } = useUsuario();
+  const paisesArray = Object.keys(paises);
 
   return (
     <Formik
       initialValues={{
         mini_biografia: usuario.mini_biografia,
+        nome: usuario.nome,
+        email: usuario.email,
+        senha: usuario.senha,
+        data_nascimento: usuario.data_nascimento,
+        genero: usuario.genero,
+        pais: usuario.pais,
+        estado: usuario.estado,
+        cidade: usuario.cidade,
+        genero_favorito: usuario.genero_favorito,
+        genero_favorito_porque: usuario.genero_favorito_porque,
+        genero_n_favorito: usuario.genero_n_favorito,
+        assisto_para: usuario.assisto_para,
       }}
     >
-      <Form autoComplete="off">
-        <TextField1
-          label="Biografia"
-          multiline
-          rows={6}
-          id="mini_biografia"
-          fullWidth
-          placeholder="Insira um biografia curta e descritiva."
-        />
-        <Box mt={1} display="flex" justifyContent={"center"}>
-          <Button
-            style={{ height: "42px" }}
-            variant="contained"
-            color="primary"
-          >
-            Concluir
-          </Button>
-        </Box>
-      </Form>
+      {({ values }) => (
+        <Form autoComplete="off">
+          <TextField1
+            placeholder="Insira seu nome"
+            icon={<FaUser />}
+            id="nome"
+            label="Nome de usuário"
+          />
+          <Box my={2.5}>
+            <TextField1
+              placeholder="Insira seu email"
+              icon={<AlternateEmail style={{ width: 18, height: 18 }} />}
+              id="email"
+              label="Email"
+            />
+          </Box>
+
+          <Box mt={2.5}>
+            <TextField1
+              placeholder="Data de nascimento"
+              type="date"
+              icon={null}
+              id="data_nascimento"
+              label="Quando você nasceu?"
+            />
+          </Box>
+
+          <Box className="genero_e_foto" my={2.5}>
+            <Box>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">
+                  <Text variant="subtitle1" style={{ fontWeight: 500 }}>
+                    Genero
+                  </Text>
+                </FormLabel>
+                <RadioGroup aria-label="gender" id="genero" color="primary">
+                  <Box display="flex">
+                    <FormControlLabel
+                      value="m"
+                      control={<Radio color="primary" />}
+                      label="Masulino"
+                    />
+                    <FormControlLabel
+                      value="f"
+                      control={<Radio color="primary" />}
+                      label="Feminino"
+                    />
+                  </Box>
+                </RadioGroup>
+              </FormControl>
+            </Box>
+          </Box>
+
+          <Box>
+            <TextField1 select id="pais" label="Escolha o seu pais">
+              {paisesArray.map((pais) => (
+                <MenuItem key={pais} value={pais}>
+                  {pais}
+                </MenuItem>
+              ))}
+            </TextField1>
+          </Box>
+          <Box mt={2.5}>
+            <TextField1 select id="estado" label="Escolha o seu estado">
+              {Object.keys(paises[values.pais]).map((estado) => (
+                <MenuItem key={estado} value={estado}>
+                  {estado}
+                </MenuItem>
+              ))}
+            </TextField1>
+          </Box>
+          <Box mt={2.5}>
+            {paises[values.pais][values.estado] && (
+              <>
+                {paises[values.pais][values.estado].length > 0 && (
+                  <TextField1 select id="cidade" label="Escolha a sua cidade">
+                    {paises[values.pais][values.estado].map((cidade) => (
+                      <MenuItem key={cidade} value={cidade}>
+                        {cidade}
+                      </MenuItem>
+                    ))}
+                  </TextField1>
+                )}
+
+                {paises[values.pais][values.estado].length === 0 && (
+                  <TextField1 id="cidade" label="Nome da sua cidade" />
+                )}
+              </>
+            )}
+            {!paises[values.pais][values.estado] && (
+              <TextField1 id="cidade" label="Nome da sua cidade" />
+            )}
+          </Box>
+
+          <Box mt={2.5}>
+            <TextField1
+              select
+              id="genero_favorito"
+              label="Escolha o seu genero favorito"
+            >
+              {generos.map((genero) => (
+                <MenuItem key={genero} value={genero}>
+                  {genero}
+                </MenuItem>
+              ))}
+            </TextField1>
+          </Box>
+          <Box mt={2.5}>
+            <TextField1
+              id="genero_favorito_porque"
+              label="Gosto mais desse genero porque..."
+              placeholder="Este campo é opcional"
+            />
+          </Box>
+
+          <Box mt={2.5}>
+            <TextField1
+              select
+              id="genero_n_favorito"
+              label="O genero que menos gostas"
+            >
+              {generos
+                .filter((genero) => genero !== values.genero_favorito)
+                .map((genero) => (
+                  <MenuItem key={genero} value={genero}>
+                    {genero}
+                  </MenuItem>
+                ))}
+            </TextField1>
+          </Box>
+          <Box mt={2.5}>
+            <TextField1 select id="assisto_para" label="Assisto para...">
+              {motivos.map((motivo) => (
+                <MenuItem key={motivo} value={motivo}>
+                  {motivo}
+                </MenuItem>
+              ))}
+            </TextField1>
+          </Box>
+
+          <Box mt={4} display="flex" flexDirection="column" alignItems="center">
+            <Button
+              variant="contained"
+              style={{ width: "150px", height: "42px" }}
+              color="primary"
+              type="submit"
+            >
+              Concluido
+            </Button>
+          </Box>
+        </Form>
+      )}
     </Formik>
   );
 }
