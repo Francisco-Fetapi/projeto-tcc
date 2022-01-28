ec3079b0c87292b6f4a301b0e813c2b4953191ee
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-01-28 11:53:06 +0000

    Ajustes e mais ajustes

--- src/components/Perfil/Amigo.jsx
+++ src/components/Perfil/Amigo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import { Text } from "~/styles";
+import { primeiroEUltimoNome } from "~/helpers";
 
 export default function Amigo({ user }) {
   return (
@@ -8,7 +9,7 @@ export default function Amigo({ user }) {
       <img className="img-amigo" src={user.foto_perfil} alt="user" />
       <Box component="figcaption">
         <Text align="center" variant="body2" color="textSecondary">
-          {user.nome}
+          {primeiroEUltimoNome(user)}
         </Text>
       </Box>
     </Box>
