ae14d6cc6f59b84cfd92c2a5e8276758aa1d1e53
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-02-03 13:57:03 +0100

    Posts adicionados em POSTS GUARDADOS

--- src/components/PostSaveds/MainContent.jsx
+++ src/components/PostSaveds/MainContent.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { PostSaveds } from "~/styles/pages/PostSaveds";
 import { Text } from "~/styles";
+import Box from "@material-ui/core/Box";
+import Posts from "~/components/Posts";
 
 export default function MainContent() {
   return (
@@ -8,6 +10,12 @@ export default function MainContent() {
       <Text variant="h5" style={{ textTransform: "uppercase" }}>
         Publicações Guardadas
       </Text>
+      <Text variant="subtitle2" color="textSecondary">
+        Gerencie as publicações que guardaste pra ver mais tarde.
+      </Text>
+      <Box>
+        <Posts />
+      </Box>
     </PostSaveds.Main>
   );
 }
