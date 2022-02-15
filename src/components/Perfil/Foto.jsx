d62092961e988f00e991d03629aca119ad4cae9c
= <=>
2022-02-15 16:40:11 +0100

    Permitir buscar fotos de um usuario pelo ID 2

--- src/components/Perfil/Foto.jsx
+++ src/components/Perfil/Foto.jsx
@@ -3,11 +3,18 @@ import Box from "@material-ui/core/Box";
 
 import DeleteIcon from "@material-ui/icons/Delete";
 import EyeIcon from "@material-ui/icons/RemoveRedEye";
+import { useParams } from "react-router-dom";
 
 export default function Foto({ img, imgFull, selecionar, tempo, preview }) {
   const link = useRef();
+  const { id } = useParams();
+  console.log(id);
   return (
-    <Box component="figure">
+    <Box
+      component="figure"
+      onClick={link.current?.click()}
+      style={id ? { cursor: "pointer" } : null}
+    >
       <img src={img} alt="user" />
       <div className={`fundo-preto-1 ${preview ? "preview" : ""}`}>
         {!preview && <p>{tempo}</p>}
@@ -15,16 +22,20 @@ export default function Foto({ img, imgFull, selecionar, tempo, preview }) {
       </div>
       {!preview && (
         <div className="fundo-preto-2">
-          <DeleteIcon size="small" onClick={selecionar} />
-          <EyeIcon size="small" onClick={() => link.current.click()} />
-          <a
-            href={imgFull}
-            ref={link}
-            target="__blank"
-            style={{ display: "none" }}
-          >
-            ver imagem original
-          </a>
+          {!id && (
+            <>
+              <DeleteIcon size="small" onClick={selecionar} />
+              <EyeIcon size="small" onClick={() => link.current.click()} />
+              <a
+                href={imgFull}
+                ref={link}
+                target="__blank"
+                style={{ display: "none" }}
+              >
+                ver imagem original
+              </a>
+            </>
+          )}
         </div>
       )}
     </Box>
