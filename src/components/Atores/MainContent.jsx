00c68ac9f9e422f8ff6598d5c8c2381e0f893b76
= <=>
2022-02-17 18:02:24 +0100

    Correcoes e algumas imagens de atores adicionadas pra criar a tela ATORES

--- src/components/Atores/MainContent.jsx
+++ src/components/Atores/MainContent.jsx
@@ -1,6 +1,28 @@
 import React from "react";
-import PaginaEmConstrucao from "../PaginaEmConstrucao";
+import Box from "@material-ui/core/Box";
+import MoviesHeader from "~/components/MoviesHeader";
+// import movies from "~/mock/series.json";
+import { Movie } from "~/styles";
+import FormSearch from "../Forms/FormSearch";
 
 export default function MainContent() {
-  return <PaginaEmConstrucao pagina="Atores" />;
+  function procurar(values) {
+    console.log(values);
+  }
+  return (
+    <Movie.Content>
+      <MoviesHeader pagina="Atores">
+        Encontre aqui informações das maiores celebridades dos filmes e seriados
+        que tens visto. Clique num ator para ver mais informações sobre ele.
+      </MoviesHeader>
+      <Box my={2}>
+        <FormSearch
+          placeholder="Procure atores"
+          id="search"
+          procurar={procurar}
+        />
+      </Box>
+      {/* <MoviesList movies={movies} /> */}
+    </Movie.Content>
+  );
 }
