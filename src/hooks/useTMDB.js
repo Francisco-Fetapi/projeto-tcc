7118c9aca1a7242cedccd3252714f52b80b280dd
= <=>
2022-03-18 09:49:04 +0000

    Ajustes e mais ajustes e use TMDB api

--- src/hooks/useTMDB.js
+++ src/hooks/useTMDB.js
@@ -128,13 +128,13 @@ export default function useTMDB() {
     async getKeywordsMovie({ setLoading, setKeywords }, id_movie) {
       setLoading(true);
       const res = await TMDB.getKeywordsMovie(id_movie);
-      setKeywords(res.results || []);
+      setKeywords(res.keywords || res.results || []);
       setLoading(false);
     },
     async getKeywordsTv({ setLoading, setKeywords }, id_movie) {
       setLoading(true);
       const res = await TMDB.getKeywordsTv(id_movie);
-      setKeywords(res.results || []);
+      setKeywords(res.keywords || res.results || []);
       setLoading(false);
     },
     async getImagesMovie({ setLoading, setImages }, id_movie) {
