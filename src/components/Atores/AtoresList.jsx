9f60ea09c85abfaca5f8aed56f0f73c95ee42d36
= <=>
2022-03-28 18:22:44 +0100

    ajustes e mais ajustes

--- src/components/Atores/AtoresList.jsx
+++ src/components/Atores/AtoresList.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { Atores } from "~/styles/pages/Atores";
 import AtorItem from "./AtorItem";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Box from "@material-ui/core/Box";
 
 export default function AtoresList({ atores }) {
   return (
--- src/styles/pages/Atores.js
+++ src/styles/pages/Atores.js
@@ -2,39 +2,39 @@ import styled from "styled-components";
 
 export const Atores = {
   Container: styled.div``,
-  List:styled.div`
-    display:grid;
-    grid-template-columns: repeat(auto-fit,minmax(110px,130px));
-    gap:50px 20px;
+  List: styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(110px, 130px));
+    gap: 60px 10px;
     justify-content: center;
     margin-top: 50px;
   `,
-  Item:styled.figure`
-    width:100%;
-    height:140px;
-    
-    display:flex;
+  Item: styled.figure`
+    width: 100%;
+    height: 140px;
+
+    display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    margin:0px !important;
-    cursor:pointer;
-    transition: transform .4s ease-in-out;
-    :hover{
+    margin: 0px !important;
+    cursor: pointer;
+    transition: transform 0.4s ease-in-out;
+    :hover {
       transform: scale(1.05);
     }
-    :active{
-      opacity:.8;
+    :active {
+      opacity: 0.8;
     }
-    img{
-      width:100%;
-      height:100%;
+    img {
+      width: 100%;
+      height: 100%;
       border-radius: 50%;
-      object-fit:cover;
+      object-fit: cover;
     }
-    figcaption{
+    figcaption {
       margin-top: 10px;
-      text-align:center;
+      text-align: center;
     }
-  `
+  `,
 };
