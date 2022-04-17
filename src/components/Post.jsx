0b7f4b90e84a683732fb8f17ec9ff486498e2cca
= <=>
2022-04-17 08:24:41 +0100

    modelando o componente post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,6 +3,7 @@ import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
+import CardActionArea from "@material-ui/core/CardActionArea";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
@@ -16,7 +17,7 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 
-import { Text } from "../styles";
+import { Text, Link } from "../styles";
 import { FaClock, FaGlobe, FaUsers } from "react-icons/fa";
 
 import Skeleton from "@material-ui/lab/Skeleton";
@@ -24,7 +25,7 @@ import { primeiroEUltimoNome } from "~/helpers";
 
 export function SubHeader({ tempo, publico }) {
   return (
-    <Box display="flex" alignItems="center">
+    <Box display="flex" alignItems="center" style={{ zoom: 0.85 }}>
       <Box display="flex" alignItems="center">
         <FaClock />
         <Text variant="subtitle2" style={{ paddingLeft: 5 }}>
@@ -44,6 +45,13 @@ export function SubHeader({ tempo, publico }) {
 export default function Post({ children, post, posts }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const a_carregar = !posts.data.length;
+  const tem_movie_relacionado = post?.id_movie !== 0;
+  const link = {
+    movie: "filmes",
+    tv: "series",
+  };
+
+  console.log(post?.id_post, post);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -56,86 +64,137 @@ export default function Post({ children, post, posts }) {
   return (
     <Box my={2}>
       <Card className="post">
-        <CardHeader
-          avatar={
-            !a_carregar ? (
-              <img
-                className="foto-user"
-                src={post?.usuario.foto_perfil}
-                alt="foto do usuario"
-              />
-            ) : (
-              <Skeleton variant="circle" width={45} height={45} />
-            )
-          }
-          action={
-            !a_carregar ? (
-              <IconButton onClick={handleClick}>
-                <MoreVertIcon />
-              </IconButton>
-            ) : null
-          }
-          title={
-            !a_carregar ? (
-              <Text>{primeiroEUltimoNome(post.usuario)}</Text>
-            ) : (
-              <Skeleton variant="text" width="40%" />
-            )
-          }
-          subheader={
-            !a_carregar ? (
-              <SubHeader
-                tempo={post.tempo}
-                publico={post.publico ? "publico" : "amigos"}
-              />
-            ) : (
-              <Skeleton variant="text" width="20%" />
-            )
-          }
-        />
-        <CardContent>
-          {!a_carregar ? (
-            <Typography variant="body2" color="textSecondary" component="p">
-              {children}
-            </Typography>
-          ) : (
-            <Skeleton variant="rect" width="90%" height="95px" />
-          )}
-          {!a_carregar && post.imagens?.length && (
-            <Box mt={3} className="imagem-post">
-              <img src={`/img/matrix.jpg`} alt="imagem" />
-            </Box>
-          )}
-        </CardContent>
-        <Box className="post-acoes">
-          <Box ml={2} mt={2} display="flex" justifyContent="space-between">
-            {!a_carregar && post.num_reacoes > 0 && (
-              <Box display="flex" alignItems="center">
-                <Box>
-                  <img
-                    src="/img/like.png"
-                    alt="like"
-                    style={{ filter: "hue-rotate(-45deg)" }}
-                  />
-                  <img
-                    src="/img/love.png"
-                    alt="love"
-                    style={{ filter: "hue-rotate(35deg)" }}
-                  />
+        <CardActionArea>
+          <CardHeader
+            // style={{ marginRight: 8 }}
+            avatar={
+              !a_carregar ? (
+                <img
+                  className="foto-user"
+                  src={post?.usuario.foto_perfil}
+                  alt="foto do usuario"
+                />
+              ) : (
+                <Skeleton variant="circle" width={45} height={45} />
+              )
+            }
+            action={
+              !a_carregar ? (
+                <IconButton
+                  onClick={handleClick}
+                  style={{ position: "relative", zIndex: 2 }}
+                >
+                  <MoreVertIcon />
+                </IconButton>
+              ) : null
+            }
+            title={
+              !a_carregar ? (
+                <Box display="flex" alignItems="center">
+                  <Text variant="subtitle2">
+                    {primeiroEUltimoNome(post.usuario)}
+                  </Text>
+                  {tem_movie_relacionado && (
+                    <Text variant="subtitle2">
+                      &nbsp; - &nbsp;
+                      <Link
+                        // nostyle
+                        style={{ textDecoration: "none" }}
+                        to={`${link[post.media_type]}/${post.id_movie}`}
+                      >
+                        {post.name}
+                      </Link>
+                    </Text>
+                  )}
                 </Box>
-                <Box ml={0.5}>
-                  <Text>{post.num_reacoes}</Text>
-                </Box>
+              ) : (
+                <Skeleton variant="text" width="40%" />
+              )
+            }
+            subheader={
+              !a_carregar ? (
+                <SubHeader
+                  tempo={post.tempo}
+                  publico={post.publico ? "publico" : "amigos"}
+                />
+              ) : (
+                <Skeleton variant="text" width="20%" />
+              )
+            }
+          />
+        </CardActionArea>
+        <CardActionArea style={{ paddingTop: 10, paddingBottom: 10 }}>
+          <CardContent style={{ paddingTop: 0, paddingBottom: 0 }}>
+            {!a_carregar ? (
+              <Typography variant="body2" color="textSecondary" component="p">
+                {children}
+              </Typography>
+            ) : (
+              <Skeleton variant="rect" width="90%" height="95px" />
+            )}
+            {!a_carregar && post.imagens?.length && (
+              <Box mt={3} className="imagem-post">
+                <img src={`/img/matrix.jpg`} alt="imagem" />
               </Box>
             )}
-            {!a_carregar && post.num_comentarios > 0 && (
-              <Box mr={2}>
-                <Text color="textSecondary" variant="subtitle2">
-                  {post.num_comentarios} comentarios
-                </Text>
-              </Box>
+          </CardContent>
+        </CardActionArea>
+
+        <Box className="post-acoes">
+          {/* <CardActionArea style={{ paddingTop: 4, paddingBottom: 4 }}> */}
+          {/* {!a_carregar && post.num_reacoes > 0 && ( */}
+          <Box
+            style={{ zoom: 0.8 }}
+            display="flex"
+            justifyContent="space-between"
+          >
+            {!a_carregar && post.num_reacoes > 0 && (
+              <CardActionArea style={{ paddingTop: 4, paddingBottom: 4 }}>
+                <Box ml={2} display="flex" alignItems="center">
+                  <Box>
+                    <img
+                      src="/img/like.png"
+                      alt="like"
+                      style={{ filter: "hue-rotate(-45deg)" }}
+                    />
+                    <img
+                      src="/img/love.png"
+                      alt="love"
+                      style={{ filter: "hue-rotate(35deg)" }}
+                    />
+                  </Box>
+                  <Box ml={0.8} mt={-0.35}>
+                    <Text>{post.num_reacoes}</Text>
+                  </Box>
+                </Box>
+              </CardActionArea>
+            )}
+
+            {!a_carregar && (
+              <CardActionArea style={{ paddingTop: 4, paddingBottom: 4 }}>
+                <Box mr={2}>
+                  {post.num_comentarios > 0 ? (
+                    <Text
+                      align="right"
+                      color="textSecondary"
+                      variant="subtitle2"
+                    >
+                      {post.num_comentarios} comentarios
+                    </Text>
+                  ) : (
+                    <Text
+                      align="right"
+                      color="textSecondary"
+                      variant="subtitle2"
+                    >
+                      Sem comentários
+                    </Text>
+                  )}
+                </Box>
+              </CardActionArea>
             )}
           </Box>
+          {/* </CardActionArea> */}
 
           {!a_carregar && <Divider />}
           {!a_carregar ? (
